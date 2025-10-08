e6864ddd543376bdf148dd39d351853c2c7e0001
Eliram Shemesh <eliram@MacBookPro.home>
2025-10-08 13:47:49 +0300

    run flask with debug

--- main.py
+++ main.py
@@ -127,4 +127,4 @@ def home():
 #  Run the Flask server
 # -------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
